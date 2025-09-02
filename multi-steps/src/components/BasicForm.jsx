d703ff45e02dd0d93a8e0b3b3549cab701b9e3de
Khatanbaatar A <25lp5598@nest.edu.mn>
2025-09-02 11:35:11 +0800

    angi deer ogloo uzsen zuil

--- multi-steps/src/components/BasicForm.jsx
+++ multi-steps/src/components/BasicForm.jsx
@@ -1,4 +1,44 @@
+import { useState } from "react";
+
 export const BasicForm = ({ form, onChange, onChangeStep }) => {
+  const [errors, setErrors] = useState({});
+  function goToNext() {
+    const newErrors = {};
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (emailRegex.test(form.email)) {
+      newErrors.email = null;
+    } else {
+      newErrors.email = "Wrong email";
+    }
+
+    const mnPhoneRegex = /^\d{8}$/;
+
+    if (mnPhoneRegex.test(form.phone)) {
+      newErrors.phone = null;
+    } else {
+      newErrors.phone = "Wrong number";
+    }
+
+    if (form.email === "") {
+      newErrors.email = "You must enter your email";
+    } else {
+      newErrors.email = null;
+    }
+
+    if (form.phone === "") {
+      newErrors.phone = "You must enter your phone number";
+    } else {
+      newErrors.phone = null;
+    }
+    setErrors(newErrors);
+    console.log(newErrors);
+
+    if (!newErrors.email && !newErrors.phone) {
+      onChangeStep("image");
+    }
+  }
+  console.log(errors);
   return (
     <div className="m-8">
       <div className="mb-4">
@@ -13,6 +53,7 @@ export const BasicForm = ({ form, onChange, onChangeStep }) => {
             })
           }
         />
+        {errors.email && <div className="text-red-500">{errors.email}</div>}
       </div>
       <div className="mb-4">
         Phone number <br />
@@ -26,10 +67,11 @@ export const BasicForm = ({ form, onChange, onChangeStep }) => {
             })
           }
         />
+        {errors.phone && <div className="text-red-500">{errors.phone}</div>}
       </div>
       <div>
         <button
-          onClick={() => onChangeStep("image")}
+          onClick={goToNext}
           className="bg-sky-800 text-white p-4 rounded-2xl hover:bg-sky-400 hover:text-black"
         >
           Submit
