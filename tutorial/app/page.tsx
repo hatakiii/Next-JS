9093225662c6c16c98f96e954a3c816ed1e5aee4
Khatanbaatar A <25lp5598@nest.edu.mn>
2025-09-08 12:26:58 +0800

    tutorial

---
+++ tutorial/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import { MovieCard } from "@/components/my";
+export default function Home() {
+  return (
+    <div>
+      <Button variant="outline">Button</Button>
+      <MovieCard />
+    </div>
+  );
+}
