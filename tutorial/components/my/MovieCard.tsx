6be507873e9ae1b05fe7360830724768b9028380
Khatanbaatar A <25lp5598@nest.edu.mn>
2025-09-08 12:41:07 +0800

    some

--- tutorial/components/my/MovieCard.tsx
+++ tutorial/components/my/MovieCard.tsx
@@ -10,15 +10,9 @@ import {
 export const MovieCard = () => {
   return (
     <div>
-      <Card>
-        <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
-          <CardAction>Card Action</CardAction>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
+      <Card className="w-[230px] bg-secondary p-0 overflow-hidden gap-2">
+        <CardContent />
+
         <CardFooter>
           <p>Card Footer</p>
         </CardFooter>
