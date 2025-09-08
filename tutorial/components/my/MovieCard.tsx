02e6d89080a9da646792759fa04886fb8eee928a
Khatanbaatar A <25lp5598@nest.edu.mn>
2025-09-08 13:16:46 +0800

    angi

--- tutorial/components/my/MovieCard.tsx
+++ tutorial/components/my/MovieCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import {
   Card,
   CardAction,
@@ -11,10 +12,20 @@ export const MovieCard = () => {
   return (
     <div>
       <Card className="w-[230px] bg-secondary p-0 overflow-hidden gap-2">
-        <CardContent />
+        <CardContent className="p-0">
+          <Image
+            src="https://m.media-amazon.com/images/M/MV5BMDNjMzcxNTEtODRjYi00MGQ4LWE3YjAtNjllYzE1OThmZmRhXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"
+            alt="Dear Santa"
+            width={230}
+            height={340}
+          />
+        </CardContent>
 
-        <CardFooter>
-          <p>Card Footer</p>
+        <CardFooter className="flex flex-col items-start p-2">
+          <CardDescription className="flex gap-2">
+            <span>6/10</span>
+          </CardDescription>
+          <CardTitle>Movie Name</CardTitle>
         </CardFooter>
       </Card>
     </div>
