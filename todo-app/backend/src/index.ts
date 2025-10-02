2932daaada734996116300ed625e672ce3827916
hatakiii <25lp5598@nest.edu.mn>
2025-10-02 21:31:16 +0800

    backend mock db

--- todo-app/backend/src/index.ts
+++ todo-app/backend/src/index.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import { nanoid } from "nanoid";
+import fs from "node:fs";
 
 const app: Application = express();
 const port = 3000;
@@ -20,8 +21,25 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World! 123 ---ABC");
 });
 
+function getTasks() {
+  if (!fs.existsSync("data.txt")) {
+    fs.writeFileSync("data.txt", JSON.stringify([]));
+    return [];
+  }
+  const data = fs.readFileSync("data.txt", "utf8");
+  return JSON.parse(data);
+}
+
+function writeTasks(tasks: { id: string; name: string }[]) {
+  fs.writeFile("data.txt", JSON.stringify(tasks, null, 2), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
 app.get("/tasks", (req: Request, res: Response) => {
-  //fetch all tasks
+  const tasks = getTasks();
   res.send(tasks);
 });
 
@@ -32,16 +50,23 @@ app.post("/tasks", (req: Request, res: Response) => {
     res.status(400).send({ message: "name is required" });
     return;
   }
+  const tasks = getTasks();
+
   tasks.unshift({ id, name });
+
+  writeTasks(tasks);
+
   res.status(201).send({ id });
 });
 
 app.delete("/tasks/:id", (req: Request, res: Response) => {
   const id = req.params.id;
   //fetch all tasks
+  const tasks = getTasks();
   console.log({ id });
-  const newTasks = tasks.filter((task) => task.id !== id);
-  tasks = newTasks;
+  const newTasks = tasks.filter((task: { id: string }) => task.id !== id);
+  writeTasks(newTasks);
+  // tasks = newTasks;
 
   res.sendStatus(204); // No content
 });
@@ -49,9 +74,11 @@ app.delete("/tasks/:id", (req: Request, res: Response) => {
 app.put("/tasks/:id", (req: Request, res: Response) => {
   const id = req.params.id;
   const { name } = req.body;
+  const tasks = getTasks();
   //fetch all tasks
-  const index = tasks.findIndex((task) => task.id === id);
+  const index = tasks.findIndex((task: { id: string }) => task.id === id);
   tasks[index].name = name;
+  writeTasks(tasks);
   res.sendStatus(204);
 });
 
